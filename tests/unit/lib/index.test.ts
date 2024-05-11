2706e0ad10742ef5eefce128ab59b69474230740
Omar Baldi <omar.baldi.business@gmail.com>
2024-05-11 13:13:08 +0200

    Added test to assert correct behaviour when exit the retry process on condition

--- tests/unit/lib/index.test.ts
+++ tests/unit/lib/index.test.ts
@@ -1,5 +1,6 @@
 import {
   AllRetriesFailedError,
+  ExitConditionMetError,
   RetryError,
   RetryManuallyStoppedError,
   RetryPromiseHandler,
@@ -129,146 +130,36 @@ describe("Retry promise handler", () => {
     );
   });
 
-  describe.skip("Retry process with custom backOff properties", () => {
-    describe("When providing 'LINEAR' backOff property", () => {});
+  it("retry process exit condition met", async () => {
+    const mockFailedRetryProcess = vi.fn();
+    const mockPromise = () => Promise.reject<string>("Promise rejected");
+    const retryPromiseHandler = new RetryPromiseHandler(mockPromise, {
+      retries: 4,
+      backOffAmount: 500,
+      onFailedRetryProcess: mockFailedRetryProcess,
+      shouldRetryOnCondition: vi
+        .fn()
+        .mockImplementation((err: RetryError) => err.retriesMade <= 2),
+    });
 
-    describe("When providing 'FIXED' backOff property", () => {});
+    retryPromiseHandler.start();
+    await flushPromises();
+    expect(mockFailedRetryProcess).not.toHaveBeenCalled();
 
-    describe("When providing 'EXPONENTIAL' backOff property", () => {});
+    await vi.advanceTimersByTimeAsync(500);
+    expect(mockFailedRetryProcess).not.toHaveBeenCalled();
 
-    describe("When providing 'CUSTOM' backOff property", () => {});
+    await vi.advanceTimersByTimeAsync(500);
+    expect(mockFailedRetryProcess).toHaveBeenCalled();
+    const [[expectedError]] = mockFailedRetryProcess.mock.calls;
+    expect(expectedError).toBeInstanceOf(ExitConditionMetError);
+    expect(expectedError).toEqual(
+      expect.objectContaining({
+        nativeError: expect.any(Error),
+        reason: "Exit condition met",
+        retriesMade: 3,
+        retriesRemaining: 1,
+      })
+    );
   });
-
-  // describe("Promise rejected", () => {
-  //   const mockPromiseReject = vi.fn().mockImplementation(() => {
-  //     return Promise.reject("Promise rejected");
-  //   });
-
-  //   it("should invoke 'onRetryLimitExceeded' when all retries have failed", async () => {
-  //     const onRetryLimitExceededMockFn = vi.fn();
-  //     const config = {
-  //       backOffAmount: 500,
-  //       retries: 3,
-  //       onRetryLimitExceeded: onRetryLimitExceededMockFn,
-  //     } satisfies Configuration;
-  //     const retryPromiseHandler = new RetryPromiseHandler(mockPromiseReject, config);
-  //     retryPromiseHandler.start();
-
-  //     await flushPromises();
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(2);
-  //     expect(onRetryLimitExceededMockFn).not.toHaveBeenCalled();
-  //     vi.advanceTimersByTime(500);
-
-  //     await flushPromises();
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(1);
-  //     expect(onRetryLimitExceededMockFn).not.toHaveBeenCalled();
-  //     vi.advanceTimersByTime(500);
-
-  //     await flushPromises();
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(0);
-  //     expect(onRetryLimitExceededMockFn).toHaveBeenCalled();
-  //   });
-
-  //   it("should assert correct waiting behavior for 'FIXED' backOff", async () => {
-  //     const config = { backOff: "FIXED", backOffAmount: 1500 } satisfies Configuration;
-  //     const retryPromiseHandler = new RetryPromiseHandler(mockPromiseReject, config);
-  //     retryPromiseHandler.start();
-
-  //     await flushPromises();
-  //     expect(mockPromiseReject).toHaveBeenCalledTimes(1);
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(4);
-  //     expect(setTimeout).toHaveBeenCalledTimes(1);
-  //     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1500);
-  //     vi.advanceTimersByTime(1500);
-
-  //     await flushPromises();
-  //     expect(mockPromiseReject).toHaveBeenCalledTimes(2);
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(3);
-  //     expect(setTimeout).toHaveBeenCalledTimes(2);
-  //     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1500);
-  //     vi.advanceTimersByTime(1500);
-
-  //     await flushPromises();
-  //     expect(mockPromiseReject).toHaveBeenCalledTimes(3);
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(2);
-  //     expect(setTimeout).toHaveBeenCalledTimes(3);
-  //     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1500);
-  //     vi.advanceTimersByTime(1500);
-
-  //     await flushPromises();
-  //     expect(mockPromiseReject).toHaveBeenCalledTimes(4);
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(1);
-  //     expect(setTimeout).toHaveBeenCalledTimes(4);
-  //     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1500);
-  //   });
-
-  //   it("should assert correct waiting behavior for 'LINEAR' backOff", async () => {
-  //     const config = { backOff: "LINEAR", backOffAmount: 1500 } satisfies Configuration;
-  //     const retryPromiseHandler = new RetryPromiseHandler(mockPromiseReject, config);
-  //     retryPromiseHandler.start();
-
-  //     await flushPromises();
-  //     expect(mockPromiseReject).toHaveBeenCalledTimes(1);
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(4);
-  //     expect(setTimeout).toHaveBeenCalledTimes(1);
-  //     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1500);
-  //     vi.advanceTimersByTime(1500);
-
-  //     await flushPromises();
-  //     expect(mockPromiseReject).toHaveBeenCalledTimes(2);
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(3);
-  //     expect(setTimeout).toHaveBeenCalledTimes(2);
-  //     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 3000);
-  //     vi.advanceTimersByTime(3000);
-
-  //     await flushPromises();
-  //     expect(mockPromiseReject).toHaveBeenCalledTimes(3);
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(2);
-  //     expect(setTimeout).toHaveBeenCalledTimes(3);
-  //     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 4500);
-  //     vi.advanceTimersByTime(4500);
-
-  //     await flushPromises();
-  //     expect(mockPromiseReject).toHaveBeenCalledTimes(4);
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(1);
-  //     expect(setTimeout).toHaveBeenCalledTimes(4);
-  //     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 6000);
-  //   });
-
-  //   it("should assert correct waiting behavior for 'EXPONENTIAL' backOff", async () => {
-  //     const config = {
-  //       backOff: "EXPONENTIAL",
-  //       backOffAmount: 10,
-  //     } satisfies Configuration;
-  //     const retryPromiseHandler = new RetryPromiseHandler(mockPromiseReject, config);
-  //     retryPromiseHandler.start();
-
-  //     await flushPromises();
-  //     expect(mockPromiseReject).toHaveBeenCalledTimes(1);
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(4);
-  //     expect(setTimeout).toHaveBeenCalledTimes(1);
-  //     expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 10);
-  //     vi.advanceTimersByTime(10);
-
-  //     await flushPromises();
-  //     expect(mockPromiseReject).toHaveBeenCalledTimes(2);
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(3);
-  //     expect(setTimeout).toHaveBeenCalledTimes(2);
-  //     expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 100);
-  //     vi.advanceTimersByTime(100);
-
-  //     await flushPromises();
-  //     expect(mockPromiseReject).toHaveBeenCalledTimes(3);
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(2);
-  //     expect(setTimeout).toHaveBeenCalledTimes(3);
-  //     expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 1000);
-  //     vi.advanceTimersByTime(1000);
-
-  //     await flushPromises();
-  //     expect(mockPromiseReject).toHaveBeenCalledTimes(4);
-  //     expect(retryPromiseHandler.retriesRemaining).toBe(1);
-  //     expect(setTimeout).toHaveBeenCalledTimes(4);
-  //     expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 10000);
-  //   });
-  // });
 });
